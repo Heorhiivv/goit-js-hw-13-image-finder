a1aae6e629a1b7182fb96fe7ed552070a291ccce
Heorhiivv <georgevbuisness@gmail.com>
2020-11-04 01:09:01 +0200

    added pnotify

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,4 +1,17 @@
 import galleryCard from './templates/galleryCard.hbs';
+import '@pnotify/core/dist/BrightTheme.css';
+import { alert, Stack } from "@pnotify/core";
+
+const stackBottomModal = new Stack({
+  dir1: "up", // With a dir1 of "up", the stacks will start appearing at the bottom.
+  // Without a `dir2`, this stack will be horizontally centered, since the `dir1` axis is vertical.
+  firstpos1: 20, // The notices will appear 25 pixels from the bottom of the context.
+  // Without a `spacing1`, this stack's notices will be placed 25 pixels apart.
+  push: "top", // Each new notice will appear at the bottom of the screen, which is where the "top" of the stack is. Other notices will be pushed up.
+  modal: true, // When a notice appears in this stack, a modal overlay will be created.
+  overlayClose: true, // When the user clicks on the overlay, all notices in this stack will be closed.
+  context: document.querySelector(".gallery") // The notices will be placed in the "page-container" element.
+});
 
 const refs = {
   formArea: document.querySelector('#search-form'),
@@ -15,7 +28,18 @@ const refs = {
     refs.gallery.innerHTML = "";
     refs.page = 1;
     if (!refs.formArea.query.value) {
+      alert({
+        title: "Alert",
+        text: "You should write a correct category!",
+        width: "auto",
+        type: ["error"],
+        stack: stackBottomModal,
+      });
       
+      // alert({
+      //   text: 'You should write a correct category!',
+      //   stack: myStack
+      // });
       return
     }
     refs.handleSearchImg();
